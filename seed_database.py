24d4dc54e13ddf48b023f64dd792a9579bcacd02
AndresitOort <a.ortizj@uniandes.edu.co>
2026-08-29 16:21:57 -0600

    JSONCHANGE

--- seed_database.py
+++ seed_database.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import json
+import sqlite3
+from pathlib import Path
+
+# Creamos el archivo .csv
 
 datos = {
     "order_id": [100, 100, 101, 102, 103, 104],
@@ -25,4 +30,34 @@ datos = {
 df = pd.DataFrame(datos)
 df.to_csv("Data/shipments.csv", index=False)
 
-print(df)
+#print(df)
+
+# Creamos el Archivo .json:
+
+STORAGE_ADDRESS = Path(__file__).resolve().parent / "Data"
+STORAGE_ADDRESS.mkdir(parents=True, exist_ok=True)  # Crea la carpeta Data si no existe
+
+json_path = STORAGE_ADDRESS / "carriers.json"
+
+with open(json_path, "r", encoding="utf-8") as file:
+    carriers_data = json.load(file)
+
+conn = sqlite3.connect("app.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS carriers (
+    code TEXT PRIMARY KEY,
+    carrier_name TEXT NOT NULL,
+    country TEXT NOT NULL
+)
+""")
+
+for code, details in carriers_data.items():
+    cursor.execute("""
+    INSERT OR REPLACE INTO carriers (code, carrier_name, country)
+    VALUES (?, ?, ?)
+    """, (code, details["carrier_name"], details["country"]))
+
+conn.commit()
+conn.close()
